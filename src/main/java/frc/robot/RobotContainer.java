068e296c01f293a1929251e318787aed959e9365
levi Fitzpatrick <levifitzpatrick1@gmail.com>
2025-03-30 10:56:57 -0400

    updated all paths to normalized poses

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,6 @@
 package frc.robot;
 
 import java.io.File;
-import java.util.function.Supplier;
-
-import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.hardware.CANdi;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
@@ -16,11 +13,9 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Commands.AlgaeIntakeCmd;
 import frc.robot.Commands.BasicCmd;
 import frc.robot.Commands.GroundIntakeCmd;
